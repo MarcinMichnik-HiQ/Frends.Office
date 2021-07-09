270cc14b24e721ce3ed2e8546c66e12efd20285c
Marcin <marcinmichnik1@gmail.com>
2021-07-09 10:00:38 +0200

    :sparkles: segregate interface

---
+++ Frends.Office/Interfaces/IWriteExcelFileInput.cs
@@ -0,0 +1,10 @@
+﻿using System.Data;
+
+namespace Frends.Office
+{
+    public interface IWriteFileInput
+    {
+        string StringInput { get; set; }
+        string TargetPath { get; set; }
+    }
+}
