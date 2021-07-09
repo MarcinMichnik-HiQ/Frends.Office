889aa2e693e8c747dfc8b2fefa9135a2401f283c
Marcin <marcinmichnik1@gmail.com>
2021-07-09 12:45:29 +0200

    :sparkles: word writing with newlines

--- Frends.Office/WriteWordFile/WriteWordFileTask.cs
+++ Frends.Office/WriteWordFile/WriteWordFileTask.cs
@@ -5,90 +5,37 @@ using Newtonsoft.Json.Linq;
 using System.ComponentModel;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
 
 namespace Frends.Office
 {
     /// <summary>
     /// Input for file writers.
     /// </summary>
-    public class WriteWordFileInput : IWriteExcelFileInput
+    public class WriteWordFileInput : IWriteFileInput
     {
         /// <summary>
-        /// Input csv string.
+        /// Input string data.
         /// </summary>
-        [DefaultValue("\"1;2;3\\r\\na;b;c\"")]
+        [DefaultValue("\"Test input\\r\\nNew line\"")]
         [DisplayFormat(DataFormatString = "Expression")]
-        public string csv { get; set; }
-
-        /// <summary>
-        /// Determines what character will be used for splitting based on cell in csv. Deafult is ';'.
-        /// </summary>
-        [DefaultValue("';'")]
-        [DisplayFormat(DataFormatString = "Expression")]
-        public char cellDelimiter { get; set; }
+        public string StringInput { get; set; }
 
         /// <summary>
         /// Determines what string will be used for splitting lines. Default is "\r\n".
         /// </summary>
         [DefaultValue("\"\\r\\n\"")]
         [DisplayFormat(DataFormatString = "Expression")]
-        public string lineDelimiter { get; set; }
+        public string LineDelimiter { get; set; }
 
         /// <summary>
-        /// Full path of the target file to be written. File format should be .xlsx, e.g. FileName.xlsx
+        /// Full path of the target file to be written. File format should be .docx, e.g. FileName.docx
         /// </summary>
-        [DefaultValue(@"c:\temp\file.xlsx")]
+        [DefaultValue(@"c:\file.docx")]
         [DisplayFormat(DataFormatString = "Text")]
-        public string path { get; set; }
-
-        /// <summary>
-        /// This method parses the input csv string and returns DataTable object.
-        /// </summary>
-        public DataTable CsvToDataTable()
-        {
-            List<Dictionary<string, string>> parsedResult = new List<Dictionary<string, string>>();
-            string[] records = csv.Split(new string[] { lineDelimiter }, StringSplitOptions.None);
-
-            DataTable table = new DataTable();
-            int recordsPerLine = 0;
-
-            foreach (string record in records)
-            {
-                recordsPerLine = 0;
-                string[] fields = record.Split(cellDelimiter);
-                Dictionary<string, string> recordItem = new Dictionary<string, string>();
-                int i = 0;
-                foreach (string field in fields)
-                {
-                    recordItem.Add(i.ToString(), field);
-                    i++;
-                    recordsPerLine++;
-                }
-                parsedResult.Add(recordItem);
-            }
-
-            Dictionary<string, string> row = parsedResult[0];
-            foreach (KeyValuePair<string, string> pair in row)
-            {
-                table.Columns.Add(pair.Value);
-            }
-            parsedResult.RemoveAt(0);
-
-            foreach (Dictionary<string, string> dic in parsedResult)
-            {
-                DataRow workRow = table.NewRow();
-
-                int counter = 0;
-                foreach (KeyValuePair<string, string> y in dic)
-                {
-                    workRow[counter] = y.Value;
-                    counter++;
-                }
-
-                table.Rows.Add(workRow);
-            }
-            return table;
-        }
+        public string TargetPath { get; set; }
     }
 
     /// <summary>
@@ -101,13 +48,48 @@ namespace Frends.Office
         /// </summary>
         /// <param name="input"></param>
         /// <returns>Returns JToken.</returns>
-        public static JToken WriteWordFile(WriteWordFileInput input)
+        public static JToken WriteWordFile([PropertyTab] WriteWordFileInput input)
         {
             JToken taskResponse = JToken.Parse("{}");
+            int rowCounter = 0;
+            try
+            {
+                using (WordprocessingDocument wordDocument =
+                    WordprocessingDocument.Create(input.TargetPath, WordprocessingDocumentType.Document))
+                {
+                    // Add a main document part. 
+                    MainDocumentPart mainPart = wordDocument.AddMainDocumentPart();
+
+                    // Create the document structure and add some text.
+                    mainPart.Document = new Document();
+                    Body body = mainPart.Document.AppendChild(new Body());
+                    Paragraph para = body.AppendChild(new Paragraph());
+                    Run run = para.AppendChild(new Run());
+
+                    string[] records = BreakByLines(input.StringInput, input.LineDelimiter);
+                    foreach (string record in records) {
+                        run.AppendChild(new Text(record));
+                        run.Append(new Break());
+                        rowCounter++;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Unable to build and save word file.", ex);
+            }
 
             taskResponse["message"] = "The file has been written correctly.";
+            taskResponse["savedTo"] = input.TargetPath;
+            taskResponse["rows"] = rowCounter;
 
             return taskResponse;
+        }
+
+        public static string[] BreakByLines(string input, string delimiter) {
+            string[] result = input.Split(new string[] { delimiter }, StringSplitOptions.None);
+
+            return result;
         }
     }
 }
